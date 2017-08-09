26b044aa5e42414fb1f42ec11d9361fe13629bc4
tmtrbocanegra <rabp_91@hotmail.com>
2017-08-09 16:52:23 -0500

    Actualizando

---
+++ app/scripts/controllers/fondos-edit.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inexdeoAdminApp.controller:FondosEditCtrl
+ * @description
+ * # FondosEditCtrl
+ * Controller of the inexdeoAdminApp
+ */
+angular.module('inexdeoAdminApp')
+  .controller('FondosEditCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
