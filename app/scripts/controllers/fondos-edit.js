cafe249b472cb5743ca7f32c2139aab73805e6c2
Roberto André Bocanegra Palacios <rabp_91@hotmail.com>
2017-08-09 23:08:31 -0500

    Actualizando

--- app/scripts/controllers/fondos-edit.js
+++ app/scripts/controllers/fondos-edit.js
@@ -8,10 +8,26 @@
  * Controller of the inexdeoAdminApp
  */
 angular.module('inexdeoAdminApp')
-  .controller('FondosEditCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('FondosEditCtrl', function ($scope, fondo, $uibModalInstance, InfosService) {
+    $scope.fondo = $.extend(true, {}, fondo);
+
+    $scope.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    $scope.saveFondo = function(fondo, boton) {
+        $('#' + boton).text('Guardando...');
+        $('#' + boton).addClass('disabled');
+        $('#' + boton).prop('disabled', true);
+        
+        InfosService.save(fondo, function(data) {
+            $('#' + boton).removeClass('disabled');
+            $('#' + boton).prop('disabled', false);
+            $uibModalInstance.close(data);
+        }, function(err) {
+            $('#' + boton).removeClass('disabled');
+            $('#' + boton).prop('disabled', false);
+            $uibModalInstance.close(err.data);
+        });
+    };
+});
